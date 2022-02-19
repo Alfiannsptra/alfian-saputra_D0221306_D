9251082da3b37a5cc976af824cd29c0661745195
Alfiannsptra <saputraalfian355@gmail.com>
2022-02-19 09:48:32 +0800

    algoritma dan struktur data

---
+++ day1_welcome_to_java.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day1_welcome_to_java;
+
+/**
+ *
+ * @author asus
+ */
+public class day1_welcome_to_java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+    
+}
