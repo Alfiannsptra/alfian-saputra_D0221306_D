7d379039d58aedac11f654e427975cd77ee5d2a2
Alfiannsptra <saputraalfian355@gmail.com>
2022-02-20 10:27:50 +0800

    algoritma dan struktur data

---
+++ Day2_java_stdin_and_stdout_1.java
@@ -0,0 +1,21 @@
+
+package day2_java_stdin_and_stdout_1;
+
+import java.util.Scanner;
+
+
+public class Day2_java_stdin_and_stdout_1 {
+
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int c = scan.nextInt();
+        int d = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(d);
+    }
+    
+}
