55d176371aa0a39907668aedcb2ae46a652ae324
Alfiannsptra <saputraalfian355@gmail.com>
2022-02-21 20:22:37 +0800

    algoritma dan struktur data

---
+++ Day3_java_if_else.java
@@ -0,0 +1,35 @@
+
+package day3_java_if_else;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day3_java_if_else {
+
+ 
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        if(N%2==1){
+            System.out.println("Weird");
+        }else{
+            if(2 <=N && N<=5 ){
+                System.out.println("Not Weird");
+            }else if(6 <=N && N<=20 ){
+                System.out.println("Weird");
+            }else if(N>20){
+                System.out.println("Not Weird");
+            }
+                
+        }
+
+        scanner.close();
+    }
+}
