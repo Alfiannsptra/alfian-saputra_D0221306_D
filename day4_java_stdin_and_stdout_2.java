9ca683528abd844892012f4795cda40a7946f2a0
Alfiannsptra <saputraalfian355@gmail.com>
2022-02-22 09:23:08 +0800

    algoritma dan struktur data

---
+++ day4_java_stdin_and_stdout_2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package day4_java_stdin_and_stdout_2;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author asus
+ */
+public class day4_java_stdin_and_stdout_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        sc.nextLine();
+        String s = sc.nextLine();
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+    }
+
