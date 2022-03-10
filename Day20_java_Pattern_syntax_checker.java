1726b9a73c8683809aa564cd24d25965c0cb1365
Alfiannsptra <saputraalfian355@gmail.com>
2022-03-10 20:47:11 +0800

    algoritma dan struktur data

---
+++ Day20_java_Pattern_syntax_checker.java
@@ -0,0 +1,25 @@
+package day20_java_pattern_syntax_checker;
+
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Day20_java_Pattern_syntax_checker {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            //Write your code
+            try {
+                Pattern.compile(pattern);
+                System.out.println("Valid");
+            } catch (PatternSyntaxException e) {
+                System.out.println("Invalid");
+            }
+            testCases--;
+
+        }
+    }
+
+}
