42180b7044eabcf8c1b261fd33650b87635025da
Alfiannsptra <saputraalfian355@gmail.com>
2022-03-11 10:35:48 +0800

    algoritma dan struktur data

---
+++ Day21_Java_Regex.java
@@ -0,0 +1,21 @@
+package day21_java_regex;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+public class Day21_Java_Regex {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex{
+    public String pattern = "((([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\\.){3}([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]))\\b";
+}
