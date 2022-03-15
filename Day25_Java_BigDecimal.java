d3b5c954dcbc8fd971141f6bcc23f3aeec3161a6
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-03-15 09:05:27 +0800

    algoritma dan struktur data

---
+++ Day25_Java_BigDecimal.java
@@ -0,0 +1,35 @@
+package day25_java_bigdecimal;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+public class Day25_Java_BigDecimal {
+
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n + 2];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+        //Write your code here
+        Comparator<String> komparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                BigDecimal a = new BigDecimal(s1);
+                BigDecimal b = new BigDecimal(s2);
+                return b.compareTo(a);//Membandingkan
+            }
+        };
+        Arrays.sort(s, 0, n, komparator);
+
+        //Output
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
+    }
+
+}
