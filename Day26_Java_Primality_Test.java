65de564586c8860ddb3b8361b1e32abcb5c4fb85
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-03-16 11:52:56 +0800

    algoritma dan  struktur data

---
+++ Day26_Java_Primality_Test.java
@@ -0,0 +1,28 @@
+package day26_java_primality_test;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Day26_Java_Primality_Test {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger bi = new BigInteger(n);
+        boolean prima = bi.isProbablePrime(1);
+        if (prima) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+
+}
