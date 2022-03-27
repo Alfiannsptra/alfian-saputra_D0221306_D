1bd10146ecba18e036bebdd1566be4911b9b5aae
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-03-27 17:10:15 +0800

    algoritma dan struktur data

---
+++ Day36_Java_HashSet.java
@@ -0,0 +1,29 @@
+package day36_java_hashset;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Day36_Java_HashSet {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int t = s.nextInt();
+        String[] pair_left = new String[t];
+        String[] pair_right = new String[t];
+
+        for (int i = 0; i < t; i++) {
+            pair_left[i] = s.next();
+            pair_right[i] = s.next();
+        }
+
+//Write your code here
+        HashSet<String> pairs = new HashSet<String>(t);
+
+        for (int i = 0; i < t; i++) {
+            pairs.add("(" + pair_left[i] + ", " + pair_right[i] + ")");
+            System.out.println(pairs.size());
+        }
+
+    }
+
+}
