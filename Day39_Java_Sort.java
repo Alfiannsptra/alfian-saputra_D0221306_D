6773d20a0c32e83b1e10c4e1ca8c3d0e2ac2eda2
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-03-29 15:34:10 +0800

    algoritma dan struktur data

---
+++ Day39_Java_Sort.java
@@ -0,0 +1,61 @@
+package day39_java_sort;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+class Student {
+
+    private int id;
+    private String fname;
+    private double cgpa;
+
+    public Student(int id, String fname, double cgpa) {
+        super();
+        this.id = id;
+        this.fname = fname;
+        this.cgpa = cgpa;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFname() {
+        return fname;
+    }
+
+    public double getCgpa() {
+        return cgpa;
+    }
+}
+
+public class Day39_Java_Sort {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+
+        List<Student> studentList = new ArrayList<Student>();
+        while (testCases > 0) {
+            int id = in.nextInt();
+            String fname = in.next();
+            double cgpa = in.nextDouble();
+
+            Student st = new Student(id, fname, cgpa);
+            studentList.add(st);
+
+            testCases--;
+        }
+
+        Collections.sort(studentList, Comparator.comparing(Student::getCgpa).reversed().
+                thenComparing(Student::getFname).thenComparing(Student::getId));
+
+        for (Student st : studentList) {
+            System.out.println(st.getFname());
+        }
+    }
+
+}
