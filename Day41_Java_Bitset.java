2012461509d486fb9d060f3b975340db1c97241b
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-03-31 10:24:05 +0800

    algoritma dan struktur data

---
+++ Day41_Java_Bitset.java
@@ -0,0 +1,46 @@
+package day41_java_bitset;
+
+import java.util.BitSet;
+import java.util.Scanner;
+
+public class Day41_Java_Bitset {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        BitSet b1 = new BitSet(n);
+        BitSet b2 = new BitSet(n);
+
+        BitSet[] bitset = new BitSet[3];
+        bitset[1] = b1;
+        bitset[2] = b2;
+
+        for (int i = 0; i < m; i++) {
+            String op = sc.next();
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            switch (op) {
+                case "AND":
+                    bitset[x].and(bitset[y]);
+                    break;
+                case "OR":
+                    bitset[x].or(bitset[y]);
+                    break;
+                case "XOR":
+                    bitset[x].xor(bitset[y]);
+                    break;
+                case "FLIP":
+                    bitset[x].flip(y);
+                    break;
+                case "SET":
+                    bitset[x].set(y);
+                    break;
+            }
+            System.out.printf("%d %d%n", b1.cardinality(), b2.cardinality());
+        }
+    }
+
+}
