b2ff226ebbd9c9d8c55644fe29a8ccdd423664f0
Alfiannsptra <99625368+Alfiannsptra@users.noreply.github.com>
2022-04-12 21:48:16 +0800

    algoritma dan struktur data

---
+++ Day52_Java_Exception_Handling.java
@@ -0,0 +1,36 @@
+package day52_java_exception_handling;
+
+import java.util.Scanner;
+
+class MyCalculator {
+
+    public static int power(int n, int p) throws Exception {
+        if (n < 0 || p < 0) {
+            throw new Exception("n or p should not be negative.");
+        } else if (n == 0 && p == 0) {
+            throw new Exception("n and p should not be zero.");
+        } else {
+            return ((int) Math.pow(n, p));
+        }
+    }
+}
+
+public class Day52_Java_Exception_Handling {
+
+    public static final MyCalculator my_calculator = new MyCalculator();
+    public static final Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while (in.hasNextInt()) {
+            int n = in.nextInt();
+            int p = in.nextInt();
+
+            try {
+                System.out.println(my_calculator.power(n, p));
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+
+}
---
+++ Day53_Java_Varargs__Simple_Addition.java
@@ -0,0 +1,61 @@
+package day53_java_varargs_._simple_addition;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+class Add {
+
+    void add(int... args) {
+        int sum = 0;
+        String s = "";
+        for (int i : args) {
+            sum += i;
+            s += i + "+";
+        }
+        String out = s.substring(0, s.length() - 1) + "=" + sum;
+        System.out.println(out);
+    }
+}
+
+public class Day53_Java_Varargs__Simple_Addition {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            int n6 = Integer.parseInt(br.readLine());
+            Add ob = new Add();
+            ob.add(n1, n2);
+            ob.add(n1, n2, n3);
+            ob.add(n1, n2, n3, n4, n5);
+            ob.add(n1, n2, n3, n4, n5, n6);
+            Method[] methods = Add.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
